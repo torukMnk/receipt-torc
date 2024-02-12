5fa3c4183b968810e6c2a097cd84065668dece02
Guillermo Quezada <meemocol@gmail.com>
2024-02-12 13:12:56 -0600

    add rspec test

---
+++ spec/receipt_spec.rb
@@ -0,0 +1,38 @@
+require 'pry'
+require_relative '../receipt'
+describe '#Receipt' do
+  subject { Receipt.new(purchases) }
+  let(:purchases) { ["1 imported bottle of perfume at 27.99",
+                     "1 bottle of perfume at 18.99",
+                     "1 packet of headache pills at 9.75",
+                     "3 imported boxes of chocolates at 11.25" ]
+  }
+
+  context 'Output decorator' do
+
+    it 'must return Output and number of items' do
+      expect(subject.out_put_decorator).to eq("Output 4")
+    end
+  end
+
+  context 'Items decorator' do
+
+    it 'must return items and totals' do
+      expected_items = ["1 imported bottle of perfume: 32.19",
+                        "1 bottle of perfume: 20.89",
+                        "1 packet of headache pills: 9.75",
+                        "3 imported boxes of chocolates: 35.55"]
+
+
+      expect(subject.items_decorator).to eq(expected_items)
+    end
+  end
+
+  context 'Tax decorator' do
+    it 'must return taxes and total' do
+      subject.items_decorator
+      expect(subject.taxes_decorator).to eq("Sales Taxes: 7.9")
+      expect(subject.total_decorator).to eq("Total: 98.38")
+    end
+  end
+end
