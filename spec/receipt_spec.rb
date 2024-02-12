b9521cfe175dd3ebf257342c0670d911f076dabb
Guillermo Quezada <meemocol@gmail.com>
2024-02-12 13:39:47 -0600

    update specs

--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -1,6 +1,70 @@
 require 'pry'
 require_relative '../receipt'
-describe '#Receipt' do
+
+describe '#Receipt 1' do
+  subject { Receipt.new(purchases) }
+  let(:purchases) { ["2 book at 12.49",
+                     "1 music CD at 14.99",
+                     "1 chocolate bar at 0.85"]
+  }
+
+  context 'Items decorator' do
+    it 'must return items and totals' do
+      expected_items = ["2 book: 24.98",
+                        "1 music CD: 16.49",
+                        "1 chocolate bar: 0.85"]
+      expect(subject.items_decorator).to eq(expected_items)
+
+    end
+  end
+
+  context 'Tax decorator' do
+    it 'must return taxes and total' do
+      subject.items_decorator
+      expect(subject.taxes_decorator).to eq("Sales Taxes: 1.5")
+    end
+  end
+
+  context 'Total decorator' do
+    it 'must return total' do
+      subject.items_decorator
+      expect(subject.total_decorator).to eq("Total: 42.32")
+    end
+  end
+end
+
+describe '#Receipt 2' do
+  subject { Receipt.new(purchases) }
+  let(:purchases) { [
+    "1 imported box of chocolates at 10.00",
+    "1 imported bottle of perfume at 47.50" ]
+  }
+
+  context 'Items decorator' do
+    it 'must return items and totals' do
+      expected_items = ["1 imported box of chocolates: 10.5",
+                        "1 imported bottle of perfume: 54.65"]
+
+      expect(subject.items_decorator).to eq(expected_items)
+    end
+  end
+
+  context 'Tax decorator' do
+    it 'must return taxes and total' do
+      subject.items_decorator
+      expect(subject.taxes_decorator).to eq("Sales Taxes: 7.65")
+    end
+  end
+
+  context 'Total decorator' do
+    it 'must return total' do
+      subject.items_decorator
+      expect(subject.total_decorator).to eq("Total: 65.15")
+    end
+  end
+end
+
+describe '#Receipt 3' do
   subject { Receipt.new(purchases) }
   let(:purchases) { ["1 imported bottle of perfume at 27.99",
                      "1 bottle of perfume at 18.99",
